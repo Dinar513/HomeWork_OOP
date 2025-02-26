e21ef2a384c3422b154fcac3e265aa2735536b0b
dinar valeev <dinarvaleev513@gmail.com>
2025-02-26 10:50:37 +0500

    15.1

--- src/classes.py
+++ src/classes.py
@@ -12,6 +12,12 @@ class Product:
         self.__price = price
         self.quantity = quantity
 
+    def __str__(self):
+        return f"{self.name}, {self.__price} руб. Остаток: {self.quantity} шт."
+
+    def __add__(self, other):
+        return self.quantity * self.__price + other.quantity * other.price
+
     @classmethod
     def new_product(cls, prod_list: list):
         name, description, price, quantity = prod_list
@@ -53,6 +59,12 @@ class Category:
         Category.total_categories += 1
         Category.total_products += len(self.__products)
 
+    def __str__(self):
+        total = 0
+        for i in self.__products:
+            total += i.quantity
+        return f"{self.name}, количество продуктов: {total} шт."
+
     def add_product(self, product):
         """добавляет новый продукт в приватный атрибут products"""
         if product is True or product is not None:
@@ -73,20 +85,6 @@ product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200M
 product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
 product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 
-# print(product1.name)
-# print(product1.description)
-# print(product1.price)
-# print(product1.quantity)
-#
-# print(product2.name)
-# print(product2.description)
-# print(product2.price)
-# print(product2.quantity)
-#
-# print(product3.name)
-# print(product3.description)
-# print(product3.price)
-# print(product3.quantity)
 
 category1 = Category(
     "Смартфоны",
@@ -94,11 +92,6 @@ category1 = Category(
     [product1, product2, product3],
 )
 
-# print(category1.name == "Смартфоны")
-# print(category1.description)
-# print(len(category1.products))
-# print(category1.total_categories)
-# print(category1.total_products)
 
 product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
 category2 = Category(
@@ -107,13 +100,6 @@ category2 = Category(
     [product4],
 )
 
-# print(category2.name)
-# print(category2.description)
-# print(len(category2.products))
-# print(category2.products)
-#
-# print(Category.total_categories)
-# print(Category.total_products)
 
 product6 = Product("55!!!!!!!!! QLED 4K", "Фоновая подсветка", -2, 1)
 product7 = Product("55&&&&&&&&&& QLED 4K", "Фоновая подсветка", 100, 1)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -18,17 +18,15 @@ def test_get_classes_from_json_1(mock_categories):
     assert get_classes_from_json(mock_categories)[0].name == "Телевизоры"
 
 
-def test_get_classes_from_json_2(mock_categories):
-    assert get_classes_from_json(mock_categories)[0].products[0].name == "55 QLED 4K"
+def test_get_classes_from_json_2(capsys, mock_categories):
+    get_classes_from_json(mock_categories)[0].products
+    message = capsys.readouterr()
+    assert message.out.strip() == "55 QLED 4K, 7 руб. Остаток: 123000"
 
 
-def test_get_classes_from_json_3(mock_categories):
-    assert get_classes_from_json(mock_categories)[0].products[0].price == 7
-
-
-def test_get_classes_from_json_4():
+def test_get_classes_from_json_3():
     assert get_classes_from_json([]) == []
 
 
-def test_get_classes_from_json_5():
+def test_get_classes_from_json_4():
     assert get_classes_from_json(None) == []
