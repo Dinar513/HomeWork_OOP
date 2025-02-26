9b4e047e07a966ab68e6131dc4e58b2a8f07967c
dinar valeev <dinarvaleev513@gmail.com>
2025-02-26 12:30:47 +0500

    f

--- src/classes.py
+++ src/classes.py
@@ -57,9 +57,9 @@ class Category:
         """добавляет новый продукт в приватный атрибут products"""
         if product is True or product is not None:
             try:
-                if isinstance(product, Product) or issubclass(product, Product):
+                if isinstance(product, Product):
                     self.__products.append(product)
-                    Category.product_count += 1
+                    Category.total_products += 1
             except Exception as e:
                 print(f'Ошибка {e}. Проверьте наследственность добавляемых классов.')
 
@@ -77,32 +77,12 @@ product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200M
 product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
 product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 
-# print(product1.name)
-# print(product1.description)
-# print(product1.price)
-# print(product1.quantity)
-#
-# print(product2.name)
-# print(product2.description)
-# print(product2.price)
-# print(product2.quantity)
-#
-# print(product3.name)
-# print(product3.description)
-# print(product3.price)
-# print(product3.quantity)
 
 category1 = Category(
     "Смартфоны",
     "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
     [product1, product2, product3],
 )
-
-# print(category1.name == "Смартфоны")
-# print(category1.description)
-# print(len(category1.products))
-# print(category1.total_categories)
-# print(category1.total_products)
 
 product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
 category2 = Category(
@@ -111,13 +91,6 @@ category2 = Category(
     [product4],
 )
 
-# print(category2.name)
-# print(category2.description)
-# print(len(category2.products))
-# print(category2.products)
-#
-# print(Category.total_categories)
-# print(Category.total_products)
 
 product6 = Product("55!!!!!!!!! QLED 4K", "Фоновая подсветка", -2, 1)
 product7 = Product("55&&&&&&&&&& QLED 4K", "Фоновая подсветка", 100, 1)
--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,6 +1,7 @@
-from src.classes import Category, category1, category2, product1, product2, Product
-
 from unittest.mock import patch
+
+from src.classes import Category, Product, category1, category2, product1, product2
+
 
 def test_product_init(product5):
     assert product5.name == "50 QLED 7K"
@@ -56,17 +57,19 @@ def test_add_product(capsys):
     category1.products
     message = capsys.readouterr()
     assert message.out.strip() == (
-        "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5\n"
-        "Iphone 15, 210000.0 руб. Остаток: 8\n"
-        "Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14\n"
-        "new_name, new_price руб. Остаток: new_quantity"
+        "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\n"
+        "Iphone 15, 210000.0 руб. Остаток: 8 шт.\n"
+        "Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт.\n"
+        "new_name, new_price руб. Остаток: new_quantity шт."
     )
 
 
 def test_property_products(capsys, category3):
     category3.products
     message = capsys.readouterr()
-    assert message.out.strip() == ("50 QLED 7K, 123000.0 руб. Остаток: 3\n" "50 QLED 54K, 200000.0 руб. Остаток: 20")
+    assert message.out.strip() == (
+        "50 QLED 7K, 123000.0 руб. Остаток: 3 шт.\n" "50 QLED 54K, 200000.0 руб. Остаток: 20 шт."
+    )
 
 
 def test_property_price(product5):
